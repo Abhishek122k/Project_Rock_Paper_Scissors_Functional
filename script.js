a1d1d9e6d04820f0d3038749c6a59653fcda6bcf
AbhishekNaik1112 <abishek.naik@kalvium.community>
2023-10-03 15:31:09 +0530

    js changes

--- script.js
+++ script.js
@@ -5,33 +5,65 @@ var computerScore = 0;
 
 var moves = ["rock", "paper", "scissors"];
 
-window.onload = function () {
-  for (let i = 0; i < 3; i++) {
-    //move buttons
-    let move = document.createElement("img");
-    move.src = moves[i] + ".png";
-    move.classList.add("move");
-    move.addEventListener("click", selectChoice);
-    document.getElementById("moves").appendChild(move);
-  }
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        // move buttons
+        let move = document.createElement("img");
+        move.src = moves[i] + ".png";
+        move.classList.add("move");
+        move.addEventListener("click", selectChoice);
+        document.getElementById("moves").appendChild(move);
+    }
 };
 
 function selectChoice() {
-  player = this.src.split("/").pop().split(".")[0]; //push png to box
-  document.getElementById("Pmove").src = this.src;
+    player = this.src.split("/").pop().split(".")[0]; // push png to box
+    document.getElementById("Pmove").src = this.src;
 
+    computer = moves[Math.floor(Math.random() * 3)]; // computer choice
+    document.getElementById("Cmove").src = computer + ".png";
 
-computer = moves[Math.floor(Math.random() * 3)]; //computer choice
-document.getElementById("Cmove").src = computer + ".png";
+    if (player == computer) {
+      playerScore+=0;       // It's a draw
+      computerScore+=0;
+      } 
+      else {
+        if (player == "rock") {
+            if (computer == "scissors") {
+                playerScore += 1;
+            } else if (computer == "paper") {
+                computerScore += 1;
+            }
+        } else if (player == "scissors") {                 //updating case scores
+            if (computer == "paper") {
+                playerScore += 1;
+            } else if (computer == "rock") {
+                computerScore += 1;
+            }
+        } else if (player == "paper") {
+            if (computer == "rock") {
+                playerScore += 1;
+            } else if (computer == "scissors") {
+                computerScore += 1;
+            }
+        }
+    }
 
-if(player==computer){
-  playerScore+=1;
-  computerScore+=1;
-  alert('Its A Draw');
+    document.querySelector(".Pscore").innerText = playerScore;
+    document.querySelector(".Cscore").innerText = computerScore;                //updating scores in score bpox
+
+    if (playerScore == 3) {
+        alert("Player wins!");
+        resetGame();                              //condition to find who ios the qinner
+    } else if (computerScore == 3) {
+        alert("Computer wins!");
+        resetGame();
+    }
 }
 
-
-
-document.querySelector(".Pscore").innerText = playerScore;
-document.querySelector(".Cscore").innerText = computerScore;
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;                                         //when score reaches 3 reset the game
+    document.querySelector(".Pscore").innerText = playerScore;
+    document.querySelector(".Cscore").innerText = computerScore;
 }

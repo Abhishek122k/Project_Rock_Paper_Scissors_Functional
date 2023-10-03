703a5c8555a3291139409a10aa8bd834f4bd4362
AbhishekNaik1112 <abishek.naik@kalvium.community>
2023-10-03 14:13:04 +0530

    minor changes

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var computerScore = 0;
 var moves = ["rock", "paper", "scissors"];
 
 window.onload = function () {
-  for (let i = 0; i < 3; i++) {   //move buttons
+  for (let i = 0; i < 3; i++) {
+    //move buttons
     let move = document.createElement("img");
     move.src = moves[i] + ".png";
     move.classList.add("move");
@@ -16,7 +17,21 @@ window.onload = function () {
 };
 
 function selectChoice() {
-   player = this.src.split("/").pop().split(".")[0]; //push png to box
+  player = this.src.split("/").pop().split(".")[0]; //push png to box
   document.getElementById("Pmove").src = this.src;
 
+
+computer = moves[Math.floor(Math.random() * 3)]; //computer choice
+document.getElementById("Cmove").src = computer + ".png";
+
+if(player==computer){
+  playerScore+=1;
+  computerScore+=1;
+  alert('Its A Draw');
+}
+
+
+
+document.querySelector(".Pscore").innerText = playerScore;
+document.querySelector(".Cscore").innerText = computerScore;
 }
